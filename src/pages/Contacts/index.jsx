fe65e196bb65a4ad1e4f04da8720e185d3c3eefd
Daniel Josias <daniieljosiaslima@gmail.com>
2023-03-24 20:42:18 -0300

    feat: página de contato

--- src/pages/Contacts/index.jsx
+++ src/pages/Contacts/index.jsx
@@ -1,28 +1,60 @@
 import React from 'react'
-import { Box, Flex, VStack, Heading, Text, FormControl, FormLabel, Input, Textarea, Button } from '@chakra-ui/react'
+import { Box, Flex, Heading, Text, FormControl, FormLabel, Input, Textarea, Button, HStack, VStack, Stack } from '@chakra-ui/react'
 import { EmailIcon, PhoneIcon  } from '@chakra-ui/icons'
 import { Icon } from '@chakra-ui/react'
 import { MdLocationOn } from 'react-icons/md' 
+import Theme from '../../theme'
 
 const Contacts = () => {
+ 
   return (
     <Box bg='#F7FAFC' boxShadow='0px 1px 2px #000000 0px 1px 2px #000000'> 
-      <Flex>
+      {Theme.breakpoints.xl && 
+        <Box p='10'>
+          <Heading color='#1A202C' fontSize='3xl' p='5' display={{base: 'none', xl: 'block', lg: 'block'}}>Let’s Connect</Heading>
+          <Text color='#4A5568' w='800px' fontSize='20' p='5' display={{base: 'none', xl: 'block', lg: 'block'}}>FlexiBlog theme comes with a pre-made contact form component. You can integrate this form with serverless services such as Formspree, Getform, FormKeep and others to receive form submissions via email.</Text>
+        </Box>
+      }
+
+      <Flex w={{xl: '800px'}}>
         <VStack alignItems='start' bg='#FFFFFF' spacing='24px' m='12' p='6' borderRadius='15'>
-        <Heading color='#1A202C' fontSize='3xl'>Let’s Connect</Heading>
-        <Text color='#4A5568' fontSize='20' >FlexiBlog theme comes with a pre-made contact form component. You can integrate this form with serverless services such as Formspree, Getform, FormKeep and others to receive form submissions via email.</Text>
+          <Heading color='#1A202C' fontSize='3xl' display={{ xl: 'none', lg: 'none'}}>Let’s Connect</Heading>
+          <Text color='#4A5568' fontSize='20' display={{ xl: 'none', lg: 'none'}}>FlexiBlog theme comes with a pre-made contact form component. You can integrate this form with serverless services such as Formspree, Getform, FormKeep and others to receive form submissions via email.</Text>
+          
           <FormControl>
-            <FormLabel>Name</FormLabel>
-            <Input type='text' mb='10'/>
+            {Theme.breakpoints.xl ?
+              <HStack>
+                <Box display={{sm:'none', lg:'block', xl:'block'}}>
+                  <FormLabel>Name</FormLabel>
+                  <Input type='text' mb='10' w='352px'/>
+      
+                  <FormLabel>Company Name</FormLabel>
+                  <Input type='text' mb='10' w='352px'/>
 
-            <FormLabel>Company Name</FormLabel>
-            <Input type='text' mb='10'/>
+                </Box>
+                <Box display={{sm:'none', lg:'block', xl:'block'}}>
+                  <FormLabel>Email</FormLabel>
+                  <Input type='email' mb='10' w='352px'/>
+      
+                  <FormLabel>Phone Number</FormLabel>
+                  <Input type='text' mb='10' w='352px'/>
+                </Box>
+              </HStack>
+              :
+              <Stack display={{sm:'block', lg:'none', xl:'none'}}>
+                <FormLabel>Name</FormLabel>
+                <Input type='text' mb='10'/>
 
-            <FormLabel>Email</FormLabel>
-            <Input type='email' mb='10'/>
+                <FormLabel>Company Name</FormLabel>
+                <Input type='text' mb='10'/>
 
-            <FormLabel>Phone Number</FormLabel>
-            <Input type='text' mb='10'/>
+                <FormLabel>Email</FormLabel>
+                <Input type='email' mb='10'/>
+                
+                <FormLabel>Phone Number</FormLabel>
+                <Input type='text' mb='10'/>
+              </Stack>
+            }
 
             <FormLabel>Subject</FormLabel>
             <Input type='text' mb='10'/>
@@ -30,12 +62,12 @@ const Contacts = () => {
             <Text color='#2D3748' mb='2' fontWeight='medium'>Your Message</Text>
             <Textarea maxH='70' mb='10'/>
 
-            <Button bg='#EC7E83' _hover={{ bg: '#EC7E83' }} color='#FFFFFF' w='250px' p='6' fontWeight='medium'>Submit</Button>
+            <Button w={{base:'250px', lg: '110px', xl: '110px'}} bg='#EC7E83' _hover={{ bg: '#EC7E83' }} color='#FFFFFF' p='6' fontWeight='medium'>Submit</Button>
           </FormControl>
         </VStack>
       </Flex>
-
-      <Box bg='#F7FAFC' boxShadow='0px 1px 2px #000000 0px 1px 2px #000000' m='6'>
+      
+      <Box w={{xl: '384px', lg: '384px'}} bg='#F7FAFC' boxShadow='0px 1px 2px #000000 0px 1px 2px #000000' m='6'>
         <Heading as='h2' size='md' p='0px 25px'>Our Commitment</Heading>
         <Flex alignItems='start' bg='#FFFFFF' m='6' p='6' borderRadius='15'>
           <VStack >
@@ -44,7 +76,7 @@ const Contacts = () => {
         </Flex>
       </Box>
 
-      <Box bg='#F7FAFC' boxShadow='0px 1px 2px #000000 0px 1px 2px #000000' m='6'>
+      <Box w={{xl: '384px', lg: '384px'}} bg='#F7FAFC' boxShadow='0px 1px 2px #000000 0px 1px 2px #000000' m='6'>
         <Heading as='h2' size='md' p='0px 25px'>Art Directing</Heading>
         <Flex bg='#FFFFFF' m='6' p='6' borderRadius='15'>
           <VStack spacing='5'alignItems='flex-start'>
--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,13 @@ import { extendTheme } from '@chakra-ui/react'
 import '@fontsource/Inter/800.css'
 import '@fontsource/Inter/500.css'
 
+const breakpoints = {
+  sm: '375px', 
+  md: '48em', 
+  lg: '1360px', 
+  xl: '1440px', 
+}
+
 const Theme = extendTheme({
     fonts: {
       heading: `'Inter', sans-serif`,
@@ -13,7 +20,8 @@ const Theme = extendTheme({
             color: '#2D3748',
         }
       }
-    }
+    },
+    breakpoints
 })
 
 export default Theme 
